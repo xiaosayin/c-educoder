a6240d8fa6bb209fa777f6c1ed90ba7d066feee3
xiaosayin <50323395+xiaosayin@users.noreply.github.com>
2021-11-10 20:28:18 +0800

    Create 5.cpp

---
+++ array/5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> number_list;
+    int x;
+    while(cin >> x){
+        if(find(number_list.begin(),number_list.end(),x) == number_list.end()){
+            number_list.push_back(x);
+        }
+    }
+    
+    cout << number_list.size() << " ";
+    for(const auto &p : number_list){
+        cout << p << " ";
+    }
+    return 0;
+}
